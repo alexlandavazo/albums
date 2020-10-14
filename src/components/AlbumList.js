c265ecdf4eb0295dcf6d9be632c64c17dac5d910
Alejandro Landavazo <alejandrolandavazo@gmail.com>
2020-10-14 11:36:07 -0700

    Axios call to get a http request and save it in state.albums

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -3,11 +3,13 @@ import {View, Text} from 'react-native';
 import axios from 'axios';
 
 class AlbumList extends Component {
+  state = {albums: []};
   async componentDidMount() {
     const response = await axios.get(
       'https://rallycoding.herokuapp.com/api/music_albums',
     );
-    console.log(response.data);
+    // eslint-disable-next-line react/no-did-mount-set-state
+    this.setState({albums: response.data});
   }
 
   render() {
